126ca5414268e2edf906db1215980cd11d9c877c
yasirsaleemvws <126661150+Yasir365@users.noreply.github.com>
2024-05-16 22:32:37 +0500

    front-end structure

---
+++ Frontend/src/app/admin-pannel/admin-pannel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-pannel',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-pannel.component.html',
+  styleUrl: './admin-pannel.component.scss'
+})
+export class AdminPannelComponent {
+
+}
